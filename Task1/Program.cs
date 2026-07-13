f7cb0e90cdcf23dadf07e9db2547575a3e07f324
shaimasaid <21f21598@mec.edu.om>
2026-07-13 22:25:46 +0400

    Add Even or Odd Checker

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,20 +1,20 @@
-﻿namespace RectangleCalculator
+﻿namespace EvenorOddChecker
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter Length: ");
-            double length = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter Width: ");
-            double width = Convert.ToDouble(Console.ReadLine());
-
-            double area = length * width;
-            double perimeter = 2 * (length + width);
-
-            Console.WriteLine("Area = " + area);
-            Console.WriteLine("Perimeter = " + perimeter);
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
         }
     }
 }
