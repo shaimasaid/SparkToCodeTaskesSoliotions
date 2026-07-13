f2dddf03fe2df6840123da1d6220bec3a3557de4
shaimasaid <21f21598@mec.edu.om>
2026-07-13 22:16:40 +0400

    Add Personal Info Card

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Shaima";
+            int age = 22;
+            double height = 1.65;
+            bool isStudent = true;
+
+            Console.WriteLine("Personal Information");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Height: " + height);
+            Console.WriteLine("Student: " + isStudent);
+        }
+    }
+}
