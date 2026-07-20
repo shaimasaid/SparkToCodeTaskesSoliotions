531d4dc1709c03e62de6b537e31b988e6d71d209
shaimasaid <21f21598@mec.edu.om>
2026-07-20 20:54:56 +0400

    Add Online Store Checkout

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,31 +1,52 @@
-﻿namespace TriangleTypeClassifier
+﻿namespace OnlineStoreCheckout
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Side 1: ");
-            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter product code: ");
+            int code = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Side 2: ");
-            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter quantity: ");
+            int quantity = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Side 3: ");
-            double c = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Coupon? (yes/no): ");
+            string answer = Console.ReadLine();
 
-            if (a + b > c && a + c > b && b + c > a)
+            bool coupon = answer == "yes";
+
+            double price = 0;
+
+            switch (code)
             {
-                if (a == b && b == c)
-                    Console.WriteLine("Equilateral");
-                else if (a == b || a == c || b == c)
-                    Console.WriteLine("Isosceles");
-                else
-                    Console.WriteLine("Scalene");
+                case 1:
+                    price = 8.5;
+                    break;
+                case 2:
+                    price = 12;
+                    break;
+                case 3:
+                    price = 5;
+                    break;
+                default:
+                    Console.WriteLine("Invalid product code");
+                    return;
             }
-            else
-            {
-                Console.WriteLine("Invalid Triangle");
-            }
+
+            double subtotal = price * quantity;
+            double discount = 0;
+
+            if (coupon && subtotal > 20)
+                discount = subtotal * 0.10;
+
+            double afterDiscount = subtotal - discount;
+            double tax = afterDiscount * 0.05;
+            double total = afterDiscount + tax;
+
+            Console.WriteLine("Subtotal: " + subtotal);
+            Console.WriteLine("Discount: " + discount);
+            Console.WriteLine("Tax: " + tax);
+            Console.WriteLine("Final Total: " + total);
         }
     }
 }
